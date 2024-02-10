89243cfa797e2a9bfadfafa463b40c83886385f0
Matvey Konovalov <ok__@outlook.com>
2024-02-10 19:31:34 +0300

    Made input more convenient

--- __main__.py
+++ __main__.py
@@ -1,5 +1,19 @@
 import Substitution
 
+def Validate(message, alphabet):
+    if alphabet == alphabet.lower():
+        msg = message.lower()
+    elif alphabet == alphabet.upper():
+        msg = message.upper()
+    else:
+        msg = message
+    extra = [x for x in set(msg) if x not in alphabet]
+    for x in extra:
+        msg = msg.replace(x, '')
+
+    return msg
+
+
 msg = '''Please choose the operation:
 0: exit
 1: Simple substitution encode
@@ -8,34 +22,41 @@ msg = '''Please choose the operation:
 4: Affine decode
 5: Recurrent Affine encode
 6: Recurrent Affine decode
+7: Change alphabet
 '''
 
 if __name__ == '__main__':
     alphabet = input('Please enter the alphabet: ')
+    message = input('Please enter the message: ')
+    val = Validate(message, alphabet)
     func = int(input(msg))
     while(func != 0):
         match func:
             case 1:
-                msg, key = input('Message, key: ').split(', ')
-                print(Substitution.Simple(alphabet, msg, key))
+                key = input('Key: ')
+                print(Substitution.Simple(alphabet, val, key))
             case 2:
-                msg, key = input('Message, key: ').split(', ')
-                print(Substitution.SimpleDecode(alphabet, msg, key))
+                key = input('Key: ')
+                print(Substitution.SimpleDecode(alphabet, val, key))
             case 3:
-                msg, *keys = input('Message, keyA, keyB: ').split(', ')
+                keys = input('KeyA, keyB: ').split(', ')
                 keyA, keyB = map(int, keys)
-                print(Substitution.Affine(alphabet, msg, keyA, keyB))
+                print(Substitution.Affine(alphabet, val, keyA, keyB))
             case 4:
-                msg, *keys = input('Message, keyA, keyB: ').split(', ')
+                keys = input('KeyA, keyB: ').split(', ')
                 keyA, keyB = map(int, keys)
-                print(Substitution.AffineDecode(alphabet, msg, keyA, keyB))
+                print(Substitution.AffineDecode(alphabet, val, keyA, keyB))
             case 5:
-                msg, *keys = input('Message, keyA1, keyA2, keyB1, keyB2: ').split(', ')
+                keys = input('KeyA1, keyA2, keyB1, keyB2: ').split(', ')
                 keyA1, keyA2, keyB1, keyB2 = map(int, keys)
-                print(Substitution.AffineRec(alphabet, msg, keyA1, keyA2, keyB1, keyB2))
+                print(Substitution.AffineRec(alphabet, val, keyA1, keyA2, keyB1, keyB2))
             case 6:
-                msg, *keys = input('Message, keyA1, keyA2, keyB1, keyB2: ').split(', ')
+                keys = input('KeyA1, keyA2, keyB1, keyB2: ').split(', ')
                 keyA1, keyA2, keyB1, keyB2 = map(int, keys)
-                print(Substitution.AffineRecDecode(alphabet, msg, keyA1, keyA2, keyB1, keyB2))
-        
+                print(Substitution.AffineRecDecode(alphabet, val, keyA1, keyA2, keyB1, keyB2))
+            case 7:
+                alphabet = input('Please enter the alphabet: ')
+
+        message = input('Please enter the message: ')
+        val = Validate(message, alphabet)
         func = int(input('Operation: '))
